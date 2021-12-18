ff64740a2fbcf18106b1fa1d9f513e7b5c448ab7
yukyung123 <rladbrud12@naver.com>
2021-12-18 18:02:41 +0900

    로그인 기능 with jsonwebtoken

--- index.js
+++ index.js
@@ -24,17 +24,34 @@ mongoose
 
 app.get("/", (req, res) => res.send("Helloworld haha"));
 
-// 회원가입을 위한 route
+// register route
 app.post("/register", (req, res) => {
   // 회원가입 할때 필요한 정보들을  client에서 가져오면
   // 그것들을 데이터베이스에 넣어준다
-
-  const user = new User(req.body);
+  const user = new User(req.body); // 모든 정보들을 모델에 넣어줌
+  //save 하기 전에 암호화
   //mongoDB method
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true }); //status(200):success
   });
+});
+
+//login route
+app.post("/login", (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다",
+      });
+    }
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {});
+  });
+
+  // 비밀번호까지 있다면 토큰을 생성하기.
 });
 
 app.listen(port, () => console.log(`example  app listening on port${port}`));
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -31,6 +34,36 @@ const userSchema = mongoose.Schema({
     // 토큰 유효기간
   },
 });
+
+userSchema.pre("save", function (next) {
+  var user = this;
+  //  비밀번호를 암호화
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cd) {
+  // 들어온 plainPassword:1234567를 암호화해서 DB내의 password와 비교
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err), cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  //
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
